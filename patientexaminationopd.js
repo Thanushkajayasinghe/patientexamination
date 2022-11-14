b2771dc9bcfffe260bed35d9f16ff8f6d47a74c9
Thanushka Jayasinghe <Thanushkajayasinghe@gmail.com>
2022-11-14 13:40:41 +0530

    Add files via upload

---
+++ patientexaminationopd.js
@@ -0,0 +1,59 @@
+var Jq = jQuery.noConflict();
+
+// search only, if the regexp matches
+var HistoryArr = [
+  "Allergies", "Colds and Flu", "Conjunctivitis", "Diarrhea", "Headaches", "Mononucleosis",
+  "Stomach Aches", "Abdominal aortic aneurysm", "Acute cholecystitis", "Acute lymphoblastic leukaemia", "Benign prostate enlargement", "Bacterial vaginosis",
+  "Cellulitis", "Chest pain"
+];
+
+
+Jq("#txtAreaHistory").autocomplete({
+  position: {
+    my: "right top",
+    at: "right bottom"
+  },
+  source: function (request, response) {
+    var str = _leftMatch(request.term, Jq("#txtAreaHistory")[0]);
+    str = (str != null) ? str[0] : "";
+    response(Jq.ui.autocomplete.filter(
+      HistoryArr, str));
+  },
+  //minLength: 2,  // does have no effect, regexpression is used instead
+  focus: function () {
+    // prevent value inserted on focus
+    return false;
+  },
+  // Insert the match inside the ui element at the current position by replacing the matching substring
+  select: function (event, ui) {
+    //alert("completing "+ui.item.value);},
+    var m = _leftMatch(this.value, this)[0];
+    var beg = this.value.substring(0, this.selectionStart - m.length);
+    this.value = beg + ui.item.value + this.value.substring(this.selectionStart, this.value.length);
+    var pos = beg.length + ui.item.value.length;
+    _setCursorPosition(this, pos);
+    return false;
+  },
+  search: function (event, ui) {
+    var m = _leftMatch(this.value, this);
+    return (m != null)
+  }
+});
+
+
+// Defines for the example the match to take which is any word (with Umlauts!!).
+function _leftMatch(string, area) {
+  return string.substring(0, area.selectionStart).match(/[\wäöüÄÖÜß]+Jq/)
+}
+
+function _setCursorPosition(area, pos) {
+  if (area.setSelectionRange) {
+    area.setSelectionRange(pos, pos);
+  } else if (area.createTextRange) {
+    var range = area.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+}
